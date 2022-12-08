a227c621943f52a2895acf9693b60b8e826d3ac5
liquibot <liquibot@liquibase.org>
2022-12-08 16:54:26 +0000

    auto generated v4.18.0

--- checks_delete.sh
+++ checks_delete.sh
@@ -255,4 +255,4 @@ if [[ -n "$USEPROCEDURESCHEMA" ]]; then
 	GLOBALS+=("--use-procedure-schema=$USEPROCEDURESCHEMA")
 fi
 
-docker-entrypoint.sh "${GLOBALS[@]}" checks delete "${PARAMS[@]}"
+docker-entrypoint.sh "${GLOBALS[@]}" pro checks delete "${PARAMS[@]}"
